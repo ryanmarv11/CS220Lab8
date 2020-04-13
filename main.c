b4dfe17dd95a19d90f4d5f2c94e08397f3f2b027
Ryan Marvin <rmarvin1@binghamton.edu>
2020-04-13 10:16:40 -0400

    first and final commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <shared.h>
+
+int main()
+{
+	void *p;
+	p = logger_malloc(10 * sizeof(unsigned int));
+	logger_free(p);
+	return 0;
+}
---
+++ shared.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <shared.h>
+
+void *logger_malloc(unsigned int size)
+{
+	void *ret;
+	printf("Allocationg %u bytes...\n", size);
+	ret = malloc(size);
+	if(ret == 0){
+		printf("Allocaiton failed1 :(\n");}
+	else{
+		printf("Successfully allocated at %p\n", ret);}
+	return ret;
+
+}
+
+void logger_free(void *p)
+{
+	printf("free()ing memory at %p...", p);
+	free(p);
+	printf("DONE\n");
+}
